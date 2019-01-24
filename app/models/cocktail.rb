49cdf9c54eb490ae0cdfe80364234b4c32759505
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-01-24 11:47:50 +0100

    problem with the creation of a cocktail solved

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ApplicationRecord
-  has_many :doses, dependent: :destroy, foreign_key: :cocktails_id
+  has_many :doses, dependent: :destroy, foreign_key: :cocktails_id # de esta forma resolvemos el problema al crear dosis y cocktail
   has_many :ingredients, through: :doses
   has_many  :reviews
   validates :name, presence: true, uniqueness: true
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,5 @@
 class Dose < ApplicationRecord
-  belongs_to :cocktail, foreign_key: :cocktails_id
+  belongs_to :cocktail, foreign_key: :cocktails_id #de esta forma solucionamos el problema por el cual no podia crear un cocktel con su dosi
   belongs_to :ingredient
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
