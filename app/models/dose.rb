f3f50be485db303c980815bf81cc18fcc11aeb97
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-01-24 12:54:42 +0100

    trying to solve problem with show method, not fixed yet

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail, foreign_key: :cocktails_id #de esta forma solucionamos el problema por el cual no podia crear un cocktel con su dosi
+  # tambine pq hemos quitado la `s` de cocktails es cocktail
   belongs_to :ingredient
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
